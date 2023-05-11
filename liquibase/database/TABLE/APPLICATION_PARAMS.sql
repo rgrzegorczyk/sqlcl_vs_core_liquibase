53938c57c604b4be6d22e94315dd2a0cfe60b68a
Grzegorczyk Rafal <rgrzegorczyk@pretius.com>
2023-05-11 10:44:11 +0200

    Most actual presentation version (intended for Kscope23) + small script fix

--- liquibase/database/TABLE/APPLICATION_PARAMS.sql
+++ liquibase/database/TABLE/APPLICATION_PARAMS.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset rgrzegorczyk:APPLICATION_PARAMS runOnchange:true  context:v1 labels:jira-1
+--changeset rgrzegorczyk:APPLICATION_PARAMS runOnchange:false  context:v1 labels:jira-1
 --comment APPLICATION_PARAMS table
 create table application_params
     (code varchar2(50),
@@ -7,7 +7,7 @@ create table application_params
     );
 
 --changeset michael:APP_PARAMS_add_col_description runOnChange:false  context:v1 labels:jira-2
---comment  Added column name
+--comment  Added column description
 ALTER TABLE APPLICATION_PARAMS ADD DESCRIPTION VARCHAR2(500);
 
 
